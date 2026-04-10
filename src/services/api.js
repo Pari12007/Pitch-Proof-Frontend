099de03f0ba010d8fc3044558509b82c1705ebf0
Pari Soni <parisoni@MacBookPro.communityfibre.co.uk>
2026-04-10 12:26:19 +0100

    fin

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL?.replace(/\/$/, "") || "http://localhost:3000";
+
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
